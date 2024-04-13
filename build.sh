bbbcb44d68a1878c268bc4348a5da5e0ff77c749
fernando <nandoxp@gmail.com>
2024-04-13 08:32:57 -0300

    lab beanstalk

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
-aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin [SEU_REPOSITORIO]
+
+aws ecr get-login-password --region us-east-1  | docker login --username AWS --password-stdin 339713120966.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
-docker tag bia:latest [SEU_REPOSITORIO]/bia:$versao
-docker push [SEU_REPOSITORIO]/bia:$versao
-rm .env 2> /dev/null
+
+docker tag bia:latest 339713120966.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker push 339713120966.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+
+
+#rm .env 2> /dev/null
+rm .env
+
 ./gerar-compose.sh
 rm bia-versao-*zip
 zip -r bia-versao-$versao.zip docker-compose.yml
